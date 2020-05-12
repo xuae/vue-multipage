65f02880f6b87e5709d4efea64416a18cd3e0024
xuae <55976226+xuae@users.noreply.github.com>
2020-05-12 16:33:33 +0800

    添加多项目独立启动，批量打包

--- config/project.js
+++ config/project.js
@@ -7,54 +7,69 @@ const path = require('path');
 const glob = require('glob')
 const merge = require("webpack-merge");
 
-const PAGE_PATH = path.resolve(__dirname, '../src/projects');
+// 项目文件夹
+const PATH = path.resolve(__dirname, '../src/projects');
+// 全部项目入口 js 文件
+const entryFiles = glob.sync(PATH + '/*/main.js');
 
-const projectName = process.argv[3];
+const arg = process.argv[3];
+// 当前项目名
+const projectName = arg && arg.startsWith('--project=') ? arg.replace('--project=', '') : arg;
+
+// 全部项目名
+const projectNames = entryFiles.map(filePath => {
+  let fileList = filePath.split('/');
+  return fileList[fileList.length - 2];
+});
 
 module.exports = {
   name: projectName,
+  names: projectNames,
   pages: customConfig => {
-    let entryFiles = glob.sync(PAGE_PATH + '/*/main.js');
-    let map = {};
-    const projectNames = entryFiles.map(filePath => {
-      let fileList = filePath.split('/');
-      let fileName = fileList[fileList.length - 2];
-      return fileName;
-    });
-
+    let command = ''; // 执行的命令
     if (process.env.NODE_ENV == 'production') {
+      command = 'build';
+    } else if(process.env.NODE_ENV == 'development') {
+      command = 'serve';
+    }
+
+    let map = {};
+    if(projectName || process.env.NODE_ENV == 'production') {
       if(!projectName) {
-        throw '请输入项目名称，项目名称是 src/projects 下的文件夹名 npm run build [projectName]';
+        throw `请输入项目名称，项目名称是 src/projects 下的文件夹名 npm run ${command} [projectName]`;
       } else if(!projectNames.includes(projectName)) {
         throw '请输入正确的项目名称，项目名称是 src/projects 下的文件夹名';
       }
+
       map = {
         index: {
           // page的入口
-          entry: `${PAGE_PATH}/${projectName}/main.js`,
+          entry: `${PATH}/${projectName}/main.js`,
           // 模板来源
-          template: `${PAGE_PATH}/${projectName}/index.html`,
+          template: `${PATH}/${projectName}/index.html`,
           // 在 dist/index.html 的输出
           filename: 'index.html',
           chunks: ['chunk-vendors', 'chunk-common', 'index']
         }
       }
     } else {
-      entryFiles.forEach(filePath => {
-        let fileList = filePath.split('/');
-        let fileName = fileList[fileList.length - 2];
-
-        let conf = {
-          entry: filePath,
-          template: filePath.replace(/main.js$/, 'index.html'),
+      /**
+       * 本地启动时，未输入项目名，则启动全部
+       *
+       * 项目访问地址是：http://localhost:端口/项目名
+       */
+      projectNames.forEach(name => {
+        map[name] = {
+          // page的入口
+          entry: `${PATH}/${name}/main.js`,
+          // 模板来源
+          template: `${PATH}/${name}/index.html`,
           // 在 dist/index.html 的输出
-          filename: fileName + '.html',
+          filename: name + '.html',
           // 如果不加这行则每个页面都会引入共享的js脚本
-          chunks: ['chunk-vendors', 'chunk-common', fileName],
+          chunks: ['chunk-vendors', 'chunk-common', name],
           inject: true,
         };
-
-        map[fileName] = conf;
       });
     }
 
@@ -78,68 +93,5 @@ module.exports = {
     }
 
     return map;
-  },
-  single: customConfig => {
-    let entryFiles = glob.sync(PAGE_PATH + '/*/main.js');
-    const projectNames = entryFiles.map(filePath => {
-      let fileList = filePath.split('/');
-      let fileName = fileList[fileList.length - 2];
-      return fileName;
-    });
-
-    let command = '';
-    if (process.env.NODE_ENV == 'production') {
-      command = 'build';
-    } else if(process.env.NODE_ENV == 'development') {
-      command = 'serve';
-    }
-    if(!projectName) {
-      throw `请输入项目名称，项目名称是 src/projects 下的文件夹名 npm run ${command} [projectName]`;
-    } else if(!projectNames.includes(projectName)) {
-      throw '请输入正确的项目名称，项目名称是 src/projects 下的文件夹名';
-    }
-
-    let conf;
-    if (process.env.NODE_ENV == 'production') {
-      conf = {
-        // page的入口
-        entry: `${PAGE_PATH}/${projectName}/main.js`,
-        // 模板来源
-        template: `${PAGE_PATH}/${projectName}/index.html`,
-        // 在 dist/index.html 的输出
-        filename: 'index.html',
-        chunks: ['chunk-vendors', 'chunk-common', 'index']
-      }
-    } else {
-      conf = {
-        // page的入口
-        entry: `${PAGE_PATH}/${projectName}/main.js`,
-        // 模板来源
-        template: `${PAGE_PATH}/${projectName}/index.html`,
-        // 在 dist/index.html 的输出
-        filename: 'index.html',
-        chunks: ['chunk-vendors', 'chunk-common', 'index']
-      }
-    }
-
-    if (customConfig) {
-      conf = merge(conf, customConfig);
-    }
-
-    if (process.env.NODE_ENV === 'production') {
-      conf = merge(conf, {
-        minify: {
-          removeComments: true, // 删除 html 中的注释代码
-          // collapseWhitespace: true, // 删除 html 中的空白符
-          // removeAttributeQuotes: true // 删除 html 元素中属性的引号
-        },
-        chunksSortMode: 'manual'// 按 manual 的顺序引入
-      });
-    }
-
-    console.log(conf);
-    return {
-      index: conf
-    };
   },
 }
